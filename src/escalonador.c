0c946e63016dd81acd322d5c0a42440a6d8d2a07
Marcello Nascif <marcellobnf@gmail.com>
2023-05-13 20:43:15 -0300

    alterei o escalonador e interpretador
    comecei a criação da funcao de processos no interpretador e adaptei a escalonador e interpretador para a info.h

--- src/escalonador.c
+++ src/escalonador.c
@@ -6,19 +6,52 @@
 #include <sys/wait.h>
 
 #include "info.h"
+//#include "escalonador.h"
+
+//PROTOTYPES
+int createProcess (char *program, int index);
 
 int main(void){
 
-    size_t segmento;
+    size_t seg;
+    StrProcess *lstProcess;
 
-    StrProcesso *lstProcessos;
-
-    segmento = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(Processo), IPC_CREAT | 0666);
-    lstProcessos = shmat(segmento, 0, 0);
-
-
-
-
+    seg = shmget(SHM_KEY, MAX_PROCESSOS * sizeof(StrProcess), IPC_CREAT | 0666);
+    lstProcess = shmat(seg, 0, 0);
 
     return 0;
+}
+
+
+int createProcess(char *program, int index) {
+	char path[] = "../Programas/";
+	int pid, pidPai;
+	char * pidPaiChar;
+
+	pidPai = getpid();
+	pid = fork();
+	pidPaiChar = converteParaChar(pidPai);
+
+	if(pid < 0) { // Erro
+		printf("Erro ao criar processo filho.\n");
+    		exit(-1);
+    	}
+	else if (pid == 0) { // Filho 
+		char * args[2];
+    		args[0] = pidPaiChar;
+    		args[1] = NULL;
+		strcat(path, nomeProg);  // Programas/programa1 (Exemplo)
+        	if(execv(path, args) < 0) // Tenta executar o programa
+    			printf("Erro ao executar %s.\n", nomeProg); 
+	} 
+	else { // Pai (Pausa o programa que está executando no processo filho)
+		kill(pid, SIGSTOP);
+		sleep(1);
+	    printf("Escalonador: Criou processo filho %s com pid %d\n", nomeProg, pid);
+		printf("Escalonador: %s recebeu sinal SIGSTOP\n", nomeProg);
+		if(signal(SIGUSR1,recebeuSinalIO) == SIG_ERR){
+			printf("Processo pai recebeu sinal I/O (Chamando rotina)\n");
+		}
+	}
+	return pid;
 }
--- src/info.h
+++ src/info.h
@@ -3,17 +3,21 @@
 #define SHM_KEY 7000
 #define MAX_PROCESSOS 20
 
-struct strProcesso{
-    int indice;
-    char nomeProcesso[10];
-}; typedef struct strProcesso StrProcesso;
+enum status {
+    PRONTO, PROCESSANDO, ESPERA
+};typedef enum status Status;
 
-struct processo {
-    char * nome; // Nome do Programa
+struct strProcess{
+    int index;
+    char processName[10];
+}; typedef struct strProcess StrProcess;
+
+struct process {
+    char *name; // Nome do Programa
     int pid;
     Status status; // Estado atual do processo
-    int tempoEmExecucao;
-    int tempoEmEspera;
-    int IO; // 0: Não 1: Sim
-    struct processo * prox;
-}; typedef struct processo Processo;
+    int exectime;
+    int waittime;
+    int io; // 0: Não 1: Sim
+    //struct processo* prox;
+}; typedef struct process Process;
